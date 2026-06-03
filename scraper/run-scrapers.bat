9ea13f4e1692475852e7afeab1d8ba4464f5ea2a
nyerrr <160138914+nyerrr@users.noreply.github.com>
2026-06-03 13:01:56 +0800

    remove sensitive files from git tracking

--- scraper/run-scrapers.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%USERPROFILE%\Documents\presyo-ph\scraper"
-echo Running scrapers... >> scraper-log.txt
-date /t >> scraper-log.txt
-time /t >> scraper-log.txt
-node doe-scraper.js >> scraper-log.txt 2>&1
-node psa-excel-scraper.js >> scraper-log.txt 2>&1
-echo Done. >> scraper-log.txt
